adf20f0a5646a5e75ff68f06d96e65fcf84d896b
Aleksey Antropov <aleksey.antropov@corp.mail.ru>
2024-10-11 10:35:42 +0300

    feat: old/new ssh client version compat and the new installer

--- install.sh
+++ install.sh
@@ -2,18 +2,36 @@
 
 repo_path="${HOME}/git/unix-profiles"
 
-for file in .bash_profile .bashrc .gitconfig .tmux.conf .vimrc .vim; do
-	source_path="${repo_path}/$file"
-	ln -sf $source_path ~/.
-	test -f $source_path && chmod 640 $file
-	test -d $source_path && chmod -R 750 $file
-done
+install_a_file () {
+	path=$1
+	file=$2
+	mode=$3
 
-mkdir ~/.ssh 2> /dev/null
+	ln -sf ${repo_path}/${path}/${file} ~/${path}/.
+	chmod -R $mode ~/${path}/${file}
+}
+
+# shell
+install_a_file "" .bash_profile 640
+install_a_file "" .bashrc 640
+install_a_file "" .tmux.conf 640
+
+# git
+install_a_file "" .gitconfig 640
+
+# vim
+install_a_file "" .vimrc 640
+install_a_file "" .vim 750
+
+# ssh-clinet
+mkdir -p  ~/.ssh || true
 chmod 700 ~/.ssh
-for file in rc config; do
-	ln -sf ${repo_path}/.ssh/$file ~/.ssh/.
-done
-chmod 750  ~/.ssh/rc
-chmod 600  ~/.ssh/config
 
+install_a_file .ssh rc 700 
+install_a_file .ssh config 600
+
+if (ssh -V 2>&1 | egrep 'OpenSSH_7.4'); then
+	# it's sad
+	ln -sf ${repo_path}/.ssh/config_7.5 ~/.ssh/config
+	ln -sf ${repo_path}/.ssh/rc_7.5 ~/.ssh/rc
+fi
